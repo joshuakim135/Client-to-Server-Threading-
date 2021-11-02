e3d36c1a1da61fc3fad1cbc7b0ae50b585fbc721
Joshua Kim <joshuakim135@gmail.com>
2021-11-02 18:45:04 -0400

    git sync

--- starter_code/BoundedBuffer.h
+++ starter_code/BoundedBuffer.h
@@ -18,8 +18,9 @@ private:
 	2. The other alternative is keeping a char* for the sequence and an integer length (i.e., the items can be of variable length), which is more complicated.*/
 	// add necessary synchronization variables (e.g., sempahores, mutexes) and variables
 	mutex m;
-	condition_variable ifData;
-	condition_variable ifSlot;
+
+	condition_variable cv_bufferDataExists;
+	condition_variable cv_bufferSpaceExists;
 
 public:
 	BoundedBuffer(int _cap){
@@ -33,28 +34,28 @@ public:
 		// follow the class lecture pseudocode
 		//1. Perform necessary waiting (by calling wait on the right semaphores and mutexes),
 		unique_lock<mutex> l(m);
-		ifSlot.wait(l, [this]{return q.size() < cap;});
-
+		cv_bufferSpaceExists.wait(l, [this]{return q.size() < cap;}); // stays asleep unless q.size() is less than buffer capacity
+		
 		//2. Push the data onto the queue
 		q.push(data);
 		
 		//3. Do necessary unlocking and notification
 		l.unlock();
-		ifData.notify_one();
+		cv_bufferDataExists.notify_all();
 	}
 
 	vector<char> pop(char* buffer, int bufferCapacity){
 		//1. Wait using the correct sync variables
 		unique_lock<mutex> l(m);
-		ifData.wait(l, [this]{return q.size() > 0;});
+		cv_bufferDataExists.wait(l, [this]{return q.size() > 0;});
 
-		//2. Pop the front item of the queue.
+		//2. Pop the front item of the queue and save to var
 		vector<char> data = q.front();
 		q.pop();
 		
 		//3. Unlock and notify using the right sync variables
 		l.unlock();
-		ifSlot.notify_one();
+		cv_bufferSpaceExists.notify_all();
 
 		//4. Return the popped vector
 		return data;
